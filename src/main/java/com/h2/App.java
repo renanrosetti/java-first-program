445736968a1c36afcef6b2512b1a784d4240b6ca
renan <renanrosetti@gmail.com>
2021-04-26 14:11:09 -0300

    Atualizado

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,25 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello world!");
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        var sun = 0;
+        for(int number: numbers) {
+            sun+= number;
+
+        }
+        return sun;
     }
 }
